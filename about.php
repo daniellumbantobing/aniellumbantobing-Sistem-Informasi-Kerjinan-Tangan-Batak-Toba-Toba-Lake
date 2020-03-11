37154cd9a6355b0cb2b239ff890dc98ae69ef093
unknown <daniellumbantobing05@gmail.com>
2020-03-11 20:23:35 +0700

    initial commit

---
+++ about.php
@@ -0,0 +1,56 @@
+<?php
+    include 'header.php';
+?>
+
+<div id="page-title" style="margin-top: 50px; background: grey;border-bottom: 5px solid grey;">
+            <h2 style="margin-left:20px;"><i class="ico-home ico-white"></i>Tentang Kami</h2>
+        </div>
+<ul class="tp-hd-lft wow fadeInDown animated" data-wow-delay="0.5s">
+
+<div class="container">
+     <div class="single_grid" style="margin-top: 25px;">
+             <ul class="back">
+                    <li style="font-size: 28px; margin-left: 0px; color: #000000;"><i class="back_arrow"></i><center>Selamat Datang di Toba Lake</center></a></li>
+            </ul>
+    </div>
+</div>
+  <ul class="tp-hd-lft wow fadeInDown animated" data-wow-delay="0.5s">x
+<div class="col-md-8 col-md-offset-2" style="margin-right: -40px; background-color: silver">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+<!-- Indicators -->
+
+<!-- deklarasi carousel -->
+<div class="carousel-inner" role="listbox">
+<div class="item active">
+<img src="img/pabrik/1.jpeg" style="width: 2700px; height: 400px; margin-top: 20px">
+<div class="carousel-caption">
+
+</div>
+</div>
+
+</div>
+<!-- membuat panah next dan previous -->
+
+
+<br><br>
+<p style="font-family:Times New Roman; font-size: 20px; color: black">Toko Lake Toba ini adalah Toko Online yang bergerak di bidang Kerjaninan Tangan , Pakaian dan Aksesoris .Yang bisa digunakan untuk anak-anak , remaja , maupun orang dewasa.<br>
+Toko Lake Toba ini terletak pada Jl.Mulia Raja No.56 Balige HP.08126522934, Kabupaten Toba Samosir.</p>
+
+<center>
+<div class="col-md-12" style="background-color: #E6E6FA">
+  <h3>Lokasi Toko Toba Lake</h3>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d498.3120988082736!2d99.063816064231!3d2.3380849457401363!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x302e04641bb97993%3A0x9d550f79b9a45f0a!2sJl.+Mulia+Raja+No.56%2C+Balige%2C+Kabupaten+Toba+Samosir%2C+Sumatera+Utara+22312!5e0!3m2!1sid!2sid!4v1560691986255!5m2!1sid!2sid" width="900" height="400" frameborder="0" allowfullscreen></iframe>
+</div>
+</center>
+</div>
+</div>
+<div style="padding-top: 550px;">
+<?php
+    include 'footer.php';
+?>
+</div>
+
+
+
+
+
---
+++ admin/footer.php
@@ -0,0 +1,39 @@
+<footer>
+<div class="col-md-12" style="background-color: chocolate">
+		<div class="container" style="background-color: chocolate;" >
+				<div class="col-md-12 col-sm-12">
+						<h2 class="fh5co-footer-logo">Toba Lake</h2>
+						<div style="margin-left: -50px;">
+							<p style="margin-left: 60px; font-size: 15px; color: black">Toko Toba Lake ,Jln.Mulia Raja No.56  Balige Kabupaten Toba Samosir<br> 
+						<span class="glyphicon glyphicon-earphone span"></span> No Hp : 08126522934</p>
+					</div>
+				<div class="col-md-12" style="background-color: #cb6b04;">
+					<center><p style="color: black";><b>Copy Right D3TI18 Kel06 © 2019 </p></b></center>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</footer>
+
+
+
+
+	
+	<script src="js/jquery.min.js"></script>
+	
+	<script src="js/jquery.easing.1.3.js"></script>
+
+	<script src="js/bootstrap.min.js"></script>
+
+	<script src="js/owl.carousel.min.js"></script>
+
+	<script src="js/jquery.waypoints.min.js"></script>
+
+	<script src="js/jquery.magnific-popup.min.js"></script>
+
+	<script src="js/main.js"></script>
+
+	
+	</body>
+</html>
